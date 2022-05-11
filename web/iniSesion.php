5403a6430dece93afefd1ed0eba36b97be81e78a
Leal31 <lealemanuel31@protonmail.com>
2022-05-11 12:41:33 -0500

    protot insesion

---
+++ web/iniSesion.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <title>Document</title>
+</head>
+<body>
+    <div id="login">
+        <h3>Inicio de Sesion</h3>
+        <form action="" method="post">
+            <label for="user">Usuario:</label><br>
+            <input type="text" name="user" id="user" required autofocus><br>
+            <label for="pwk">Contraseña:</label><br>
+            <input type="password" name="pwk" id="pwk" required><br>
+            <input type="submit" value="Ingresar">
+        </form>
+    </div>
+</body>
+</html>
+
