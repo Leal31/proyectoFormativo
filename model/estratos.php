a39e2985b68e508cb61aa5534ee33d49e1a2fdd3
HeisenbergXx <mrwhite8q@gmail.com>
2022-05-15 19:36:34 -0500

    Actualizo los clases de las trabas Estratos y Genero

---
+++ model/estratos.php
@@ -0,0 +1,22 @@
+<?php 
+class Estratos{
+    private $estr_id;
+    private $estr_nombre;
+
+    public function setEstratosId($estr_id){
+        $this->estr_id= $estr_id;
+    }
+
+    public function getEstratosId(){
+        return $this->estr_id;
+    }
+
+    public function setEstrNombre($estr_nombre){
+        $this->estr_nombre= $estr_nombre;
+    }
+
+    public function getEstrNombre(){
+        return $this->estr_nombre;
+    }
+}
+?>
---
+++ model/generos.php
@@ -0,0 +1,22 @@
+<?php 
+class Generos{
+    private $gen_id;
+    private $gen_nombre;
+
+    public function setGenId($gen_id){
+        $this->gen_id= $gen_id;
+    }
+
+    public function getGenId(){
+        return $this->gen_id;
+    }
+    
+    public function setGenNombre($gen_nombre){
+        $this->gen_nombre=$gen_nombre;
+    }
+
+    public function getGenNombre(){
+        return $this->gen_nombre;
+    }
+}
+?>
